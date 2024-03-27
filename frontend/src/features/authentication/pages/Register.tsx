a38d416ccda4d4503ff96db2bebeaafa897cb6e5
TomPouv <contact@thomas-pouvreau.fr>
2024-03-27 17:11:32 +0100

    copy paste signin into register

--- frontend/src/features/authentication/pages/Register.tsx
+++ frontend/src/features/authentication/pages/Register.tsx
@@ -1,3 +1,30 @@
+import SignInForm from "../forms/SignIn";
+import Header from "../sections/Header";
+import SocialSignIn from "../sections/SocialSignIn";
+
 export default function Register() {
-  return <h1> Register</h1>;
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <Header title="Sign Up" />
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            <SignInForm />
+            <SocialSignIn />
+          </div>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already a member?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign In !
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
