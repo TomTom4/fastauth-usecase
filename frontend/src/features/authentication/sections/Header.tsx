3db5647fb85357296b030928292b6b5a105cd358
TomPouv <contact@thomas-pouvreau.fr>
2024-03-27 16:59:42 +0100

    rework tailwindui example into multiple components to ease maintenance and reusability

---
+++ frontend/src/features/authentication/sections/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header({ title }) {
+  return (
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <img
+        className="mx-auto h-10 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company"
+      />
+      <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        {title}
+      </h2>
+    </div>
+  );
+}
+
