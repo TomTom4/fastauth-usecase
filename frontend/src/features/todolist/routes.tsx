ddb9680645a0bd42f3d0658fd1b586da828a2737
TomPouv <contact@thomas-pouvreau.fr>
2024-03-29 16:53:22 +0100

    add a todolist routes that will be protected after auth's implementation is complete

---
+++ frontend/src/features/todolist/routes.tsx
@@ -0,0 +1,6 @@
+const todoListRoutes = {
+  path: "/todos",
+  element: <h2>Todolist</h2>,
+};
+
+export default todoListRoutes;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import authRoutes from "./features/authentication/routes";
+import todoListRoutes from "./features/todolist/routes";
 
 const router = createBrowserRouter([
   { path: "/", element: <div>hello world</div> },
   authRoutes,
+  todoListRoutes,
 ]);
 
 export default router;
