afdd0f754e24ebb0387812db021df81caf2f6c2f
TomPouv <contact@thomas-pouvreau.fr>
2024-04-02 17:11:10 +0200

    add basic slice

---
+++ frontend/src/features/authentication/slice.tsx
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+// Define a type for the slice state
+export interface AuthState {
+  isAuthenticated: boolean;
+  user: object;
+}
+
+// Define the initial state using that type
+const initialState: AuthState = {
+  isAuthenticated: false,
+  user: {},
+};
+
+export const authSlice = createSlice({
+  name: "authentication",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {},
+});
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectAuth = (state: RootState) => state.auth;
+
+export default authSlice.reducer;
