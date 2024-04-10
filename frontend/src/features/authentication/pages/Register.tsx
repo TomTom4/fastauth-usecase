a6e6e1778e2564c78078a135c4c37a24966ce56e
TomPouv <contact@thomas-pouvreau.fr>
2024-04-10 17:28:50 +0200

    implement navigation on authentication pages

--- frontend/src/features/authentication/pages/Register.tsx
+++ frontend/src/features/authentication/pages/Register.tsx
@@ -17,7 +17,7 @@ export default function Register() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Already a member?{" "}
             <a
-              href="#"
+              href="/auth/sign-in"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Sign in !
--- frontend/src/features/authentication/pages/SignIn.tsx
+++ frontend/src/features/authentication/pages/SignIn.tsx
@@ -17,7 +17,7 @@ export default function SignIn() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{" "}
             <a
-              href="#"
+              href="/auth/register"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Sign up !
