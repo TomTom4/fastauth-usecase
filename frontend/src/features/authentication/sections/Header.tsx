11fae9b2905669cab585a9f70d7c8813cabadaf6
TomPouv <contact@thomas-pouvreau.fr>
2024-04-10 17:30:39 +0200

    add a link back to homepage

--- frontend/src/features/authentication/sections/Header.tsx
+++ frontend/src/features/authentication/sections/Header.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Header({ title }) {
   return (
     <div className="sm:mx-auto sm:w-full sm:max-w-md">
-      <img
-        className="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
-      />
+      <Link to="/">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+      </Link>
       <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         {title}
       </h2>
     </div>
   );
 }
-
