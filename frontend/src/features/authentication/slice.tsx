f0e03ac5184b7ebf04ebc6296efc2c50841bd39c
TomPouv <contact@thomas-pouvreau.fr>
2024-04-23 17:28:12 +0200

    update slice

--- frontend/src/features/authentication/slice.tsx
+++ frontend/src/features/authentication/slice.tsx
@@ -7,12 +7,14 @@ import toast from "react-hot-toast";
 // Define a type for the slice state
 export interface AuthState {
   isAuthenticated: boolean;
+  accessToken: object;
   user: object;
 }
 
 // Define the initial state using that type
 const initialState: AuthState = {
   isAuthenticated: false,
+  accessToken: {},
   user: {},
 };
 
@@ -20,7 +22,14 @@ export const authSlice = createSlice({
   name: "authentication",
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
-  reducers: {},
+  reducers: {
+    userData(state, action) {
+      state.user = action.payload;
+    },
+    signinSuccess(state, action) {
+      state.accessToken = action.payload;
+    },
+  },
 });
 
 // Other code such as selectors can use the imported `RootState` type
@@ -31,6 +40,7 @@ export default authSlice.reducer;
 export function register(user: User) {
   return async (dispatch, getState) => {
     try {
+      dispatch(authSlice.actions.userData(user));
       await axios.post("/register", user);
       toast.success("you are registered");
     } catch (error) {
@@ -42,11 +52,14 @@ export function register(user: User) {
 export function signin(user: User) {
   return async (dispatch, getState) => {
     try {
+      dispatch(authSlice.actions.userData(user));
       const formData = new FormData();
       formData.append("grant_type", "password");
       formData.append("username", user.email);
       formData.append("password", user.password);
-      await axios.post("/signin", formData);
+      const response = await axios.post("/signin", formData);
+      dispatch(authSlice.actions.signinSuccess(response.data.accessToken));
+      console.log(response);
       toast.success("you signed in!");
     } catch (error) {
       toast.error(error.message);
